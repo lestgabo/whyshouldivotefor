c73165c21b471aa1adc72383e229129b37fcfe03
lestgabo <lestgabo@gmail.com>
2020-10-11 02:00:58 -0700

    Move local server into root folder

--- server-local.js
+++ server-local.js
@@ -1,5 +1,5 @@
 'use strict';
 
-const app = require('./server');
+const app = require('./functions/server');
 // allows localhost:9000 to be the server
 app.listen(9000, () => console.log('Local server listening on port 9000!'));
