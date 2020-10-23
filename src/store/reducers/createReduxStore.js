09e51c6ac0c2e314584ff4e5a808dd261a63c735
lestgabo <lestgabo@gmail.com>
2020-10-23 02:59:04 -0700

    Install redux, thunk, redux-firestore, react-redux-firebase

---
+++ src/store/reducers/createReduxStore.js
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { getFirestore } from 'redux-firestore';
+import { getFirebase } from 'react-redux-firebase';
+
+import rootReducer from './RootReducer.js';
+
+const initialState = {};
+
+export default () => {
+return createStore(rootReducer, initialState, compose(applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore }))));
+};
